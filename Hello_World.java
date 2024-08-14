311bf074db5e26fef7e056555e6582f844cf6804
ella-smella <elladumbrique@gmail.com>
2024-08-14 10:30:34 -1000

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -1,1 +1,19 @@
+/*
+ * This code is for the first assignment. It does two simple prints and then I will submit it! :3
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Task 1: Print out "Hello World"
+		System.out.print("Hello World");
+		
+		//Task 2: Print out "Hello World" again and then a third time on the next line
+		System.out.println("Hello World");
+		System.out.print("Hello World");
+		
+	}
+
+}
 
