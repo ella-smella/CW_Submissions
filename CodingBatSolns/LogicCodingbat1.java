2e9c0e1f0f6218edde742b903489f339e85523fc
ella-smella <elladumbrique@gmail.com>
2024-10-21 13:53:56 -1000

    Update LogicCodingbat1.java

--- CodingBatSolns/LogicCodingbat1.java
+++ CodingBatSolns/LogicCodingbat1.java
@@ -39,28 +39,17 @@ public boolean cigarParty(int cigars, boolean isWeekend) {
 //dateFashion(5, 2) → 0
 //dateFashion(5, 5) → 1
 public int dateFashion(int you, int date) {
-  if (you <= 2)
+  if (you <= 2 || date <= 2)
   {
     return 0;
   }
-  else if (date <= 2)
+  else if (you >= 8 || date >= 8)
   {
-    return 0;
+    return 2;
   }
-  else
+  else 
   {
-      if (you >= 8)
-      {
-        return 2;
-      }
-      else if (date >= 8)
-      {
-        return 2;
-      }
-      else
-      {
-        return 1;
-      }
+    return 1;
   }
 }
 
