9e02f8570e37ea9633dd935647c1286efe36e8ed
ella smella <elladumbrique@gmail.com>
2024-12-18 07:38:57 -1000

    Create Main.java
    wee woo

---
+++ MakeYourChoiceGame/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+	
+	//Creates Character objects for all the characters that appear in the game
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Calls methods from the Room class in order of their room number 
+		Room.roomZero();
+		Room.roomOne();
+		Room.roomTwo();
+		Room.roomThree();
+		Room.roomFive();
+		Room.roomSeven();
+		Room.roomEleven();
+		Room.roomSixteen();
+		Room.roomEighteen();
+		Room.roomTwenty();
+	}
+	
+	
+}
