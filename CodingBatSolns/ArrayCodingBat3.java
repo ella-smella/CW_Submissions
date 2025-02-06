67f38d68f58d3521cb48e43b6cbff30c54945383
ella smella <elladumbrique@gmail.com>
2025-02-05 17:09:05 -1000

    Update ArrayCodingBat3.java
    this was painful.

--- CodingBatSolns/ArrayCodingBat3.java
+++ CodingBatSolns/ArrayCodingBat3.java
@@ -158,3 +158,153 @@ public int[] plusTwo(int[] a, int[] b) {
   
   return arrAB;
 }
+
+//swapEnds Exercise
+public int[] swapEnds(int[] nums) {
+  int[] copyArr = new int [nums.length];
+  
+  for (int i = 0; i < nums.length; i++)
+  {
+    copyArr[i] = nums[i];
+  }
+  
+  copyArr[0] = nums[nums.length - 1];
+  copyArr[nums.length - 1] = nums[0];
+  
+  return copyArr;
+}
+
+//midThree Exercise
+public int[] midThree(int[] nums) {
+  int[] midArr = new int [3];
+  
+  midArr[0] = nums[nums.length / 2 - 1];
+  midArr[1] = nums[nums.length / 2];
+  midArr[2] = nums[nums.length / 2 + 1];
+  
+  return midArr;
+}
+
+//maxTriple Exercise 
+public int maxTriple(int[] nums) {
+  int max = 0;
+  
+  if (nums[0] > nums[nums.length / 2] && nums[0] > nums[nums.length - 1])
+  {
+    max = nums[0];
+  }
+  else if (nums[nums.length / 2] > nums[0] && nums[nums.length / 2] > nums[nums.length - 1])
+  {
+    max = nums[nums.length / 2];
+  }
+  else
+  {
+    max = nums[nums.length - 1];
+  }
+  
+  return max;
+}
+
+//frontPiece Exercise
+public int[] frontPiece(int[] nums) {
+  if (nums.length < 2)
+  {
+    return nums;
+  }
+  
+  int[] frontTwoArr = new int [2];
+  
+  frontTwoArr[0] = nums[0];
+  frontTwoArr[1] = nums[1];
+  
+  return frontTwoArr;
+}
+
+//unlucky1 Exercise
+public boolean unlucky1(int[] nums) {
+  boolean unlucky = false;
+  
+  if (nums.length < 2)
+  {
+    return unlucky;
+  }
+  
+  if (nums[0] == 1 && nums[1] == 3)
+  {
+   unlucky = true;
+  }
+  if (nums.length == 2)
+  {
+    return unlucky;
+  }
+
+  if ((nums[0] == 1 && nums[1] == 3) || (nums[1] == 1 && nums[2] == 3))
+  {
+    unlucky = true;
+  }
+  if (nums.length == 3)
+  {
+    return unlucky;
+  }
+  
+  if((nums[nums.length - 2] == 1) && nums[nums.length - 1] == 3)
+  {
+    unlucky = true;
+  }
+
+  return unlucky;
+}
+
+//make2 Exercise 
+public int[] make2(int[] a, int[] b) {
+  int[] makeTwoArr = new int [2];
+  
+  if (a.length == 0)
+  {
+    makeTwoArr[0] = b[0];
+    makeTwoArr[1] = b[1];
+  }
+  else if (a.length == 1)
+  {
+    makeTwoArr[0] = a[0];
+    makeTwoArr[1] = b[0];
+  }
+  else
+  {
+    makeTwoArr[0] = a[0];
+    makeTwoArr[1] = a[1];
+  }
+  
+  return makeTwoArr;
+}
+
+//front11 Exercise
+public int[] front11(int[] a, int[] b) {
+  if (a.length == 0 && b.length == 0)
+  {
+    int[] frontArr = {};
+    return frontArr;
+  }
+  
+  if (a.length == 0 || b.length == 0)
+  {
+    int[] frontArr = new int [1];
+    if (a.length == 0)
+    {
+      frontArr[0] = b[0];
+    }
+    else
+    {
+      frontArr[0] = a[0];
+    }
+    
+    return frontArr;
+  }
+  
+  int[] frontArr = new int[2];
+  
+  frontArr[0] = a[0];
+  frontArr[1] = b[0];
+  
+  return frontArr;
+}
